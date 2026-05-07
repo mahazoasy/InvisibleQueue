e2ec2146470e6738c48903f18b4751232c8e4646
mahazoasy <mahazoasy.h@zurcher.edu.mg>
2026-05-07 10:57:57 +0300

    Code modifier

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Gravatar from 'react-native-avatar-gravatar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {
   View,
